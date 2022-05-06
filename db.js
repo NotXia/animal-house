437da650dcdb97a1c8ef77d867e9d4f5c01a9ac8
NotXia <35894453+NotXia@users.noreply.github.com>
2022-05-06 15:11:01 +0200

    Add utente admin

--- db.js
+++ db.js
@@ -1,20 +1,39 @@
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+
+async function createAdmin() {
+    let username = "admin";
+    let password = "admin";
+    
+    let password_hash = await bcrypt.hash(password, 10);
+    
+    return {
+        username: username,
+        password: password_hash,
+    };
+}
 
 module.exports.connect = async function () {
     console.log(`Connecting to MongoDB at ${process.env.MONGODB_URL}`);
     const client = await MongoClient.connect(process.env.MONGODB_URL)
                                     .catch((err) => { throw err });
-    if (!client) { return null; }
 
     console.log(`Connecting to database ${process.env.MONGODB_DATABASE_NAME}`);
     let dbo = client.db(process.env.MONGODB_DATABASE_NAME);
     module.exports.dbo = dbo; // Per permettere di accedere alla connessione tramite il modulo
 
     // Creazione delle collezioni (se mancanti)
-    await dbo.createCollection("users").catch((err) => {
-        if (err.codeName !== "NamespaceExists") { throw err; }
+    dbo.createCollection("users", async (err) => {
+        if (err.codeName === "NamespaceExists") { return; }
+        else if (err.codeName !== "NamespaceExists") { throw err; }
+
+        await dbo.collection("users").createIndex({ "username": 1 }, { unique: true });
+        await dbo.collection("users").insertOne(await createAdmin()).catch((err) => {
+            if (err.code !== 11000) { throw err; } // Duplicato
+        });
     });
+
 
     console.log("Connected");
 }
