c0bd37cdf4ba18c00a8c796a3ce24ce657f0695b
NotXia <35894453+NotXia@users.noreply.github.com>
2022-05-06 17:45:17 +0200

    Add inizializzazione database

---
+++ db_init.js
@@ -0,0 +1,37 @@
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const ms = require("ms");
+
+async function createAdmin() {
+    let username = "admin";
+    let password = "admin";
+
+    let password_hash = await bcrypt.hash(password, 10);
+
+    return {
+        username: username,
+        password: password_hash,
+    };
+}
+
+async function init() {
+    const client = await MongoClient.connect(process.env.MONGODB_URL).catch((err) => { throw err });
+    let dbo = client.db(process.env.MONGODB_DATABASE_NAME);
+
+    dbo.createCollection("users", async function(err) {
+        if (err) { throw err; }
+    
+        await dbo.collection("users").createIndex({ "username": 1 }, { unique: true });
+        await dbo.collection("users").insertOne(await createAdmin()).catch((err) => { throw err; });
+    });
+    
+    dbo.createCollection("tokens", async function(err) {
+        if (err) { throw err; }
+
+        await dbo.collection("tokens").createIndex({ "timestamp": 1 }, { expireAfterSeconds: ms(process.env.REFRESH_TOKEN_EXP) });
+    });
+};
+
+init();
+process.exit()
