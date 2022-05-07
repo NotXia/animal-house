a899448531f5777663e73ddd0b36a76de36ce496
NotXia <35894453+NotXia@users.noreply.github.com>
2022-05-07 21:53:20 +0200

    Add separazione logica controller/middleware/route

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const db = require("./../db");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
+
+
+/* Strategia Passport per autenticazione username + password */
+passport.use("local", new LocalStrategy(
+    {
+        usernameField: "username",
+        passwordField: "password"
+    },
+    function (in_username, in_password, done) {
+        // Autenticazione dell'utente
+        db.users.findOne({ username: in_username }, function (err, user_data) {
+            if (err) { return done(err); }
+            if (!user_data) { return done(null, false); } // Non esiste l'utente
+
+            bcrypt.compare(in_password, user_data.password).then((hash_match) => {
+                if (hash_match) { return done(null, { username: user_data.username }); }
+                else { return done(null, false); }
+            });
+        });
+    }
+));
