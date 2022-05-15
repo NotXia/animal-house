cd396ec5a0c917bc5d2759a6c0c082c9ab07c4ab
NotXia <35894453+NotXia@users.noreply.github.com>
2022-05-15 10:17:10 +0200

    Add flag operatore nei token

--- middleware/local_strategy.js
+++ middleware/local_strategy.js
@@ -16,7 +16,7 @@ passport.use("local_operator", new LocalStrategy(
 
         bcrypt.compare(in_password, user_data.password).then((hash_match) => {
             if (hash_match) { 
-                return done(null, { id: user_data._id, username: user_data.username }); 
+                return done(null, { id: user_data._id, username: user_data.username, is_operator: true }); 
             }
             else { 
                 return done(null, false); 
