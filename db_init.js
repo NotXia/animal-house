104b7c074938a2c9fde76fd010825118da25c7a8
NotXia <35894453+NotXia@users.noreply.github.com>
2022-05-23 13:16:25 +0200

    Add validazione orari

--- db_init.js
+++ db_init.js
@@ -2,10 +2,28 @@ require("dotenv").config();
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const OperatorModel = require("./models/auth/operator");
+const HubModel = require("./models/services/hub");
+const { createTime } = require("./utilities");
 
 
 async function init() {
     await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.MONGODB_DATABASE_NAME}`);
+
+    const hq = await new HubModel({
+        name: "Sede centrale",
+        address: {
+            city: "Milano", street: "Via Vincenzo Monti", number: "51", postal_code: "20123"
+        },
+        opening_time: {
+            monday:     [{ start: createTime("00:00"), end: createTime("23:59") }],
+            tuesday:    [{ start: createTime("00:00"), end: createTime("23:59") }],
+            wednesday:  [{ start: createTime("00:00"), end: createTime("23:59") }],
+            thursday:   [{ start: createTime("00:00"), end: createTime("23:59") }],
+            friday:     [{ start: createTime("00:00"), end: createTime("23:59") }],
+            saturday:   [{ start: createTime("00:00"), end: createTime("23:59") }],
+            sunday:     [{ start: createTime("00:00"), end: createTime("23:59") }],
+        }
+    }).save().catch((err) => { console.log(err.message); });
 
     await new OperatorModel({
         username: "admin",
@@ -15,6 +33,15 @@ async function init() {
         surname: "Admin",
         enabled: true,
         permission: { admin: true },
+        working_time: {
+            monday:     [{ time: { start: createTime("00:00"), end: createTime("23:59") }, hub_id: hq._id }],
+            tuesday:    [{ time: { start: createTime("00:00"), end: createTime("23:59") }, hub_id: hq._id }],
+            wednesday:  [{ time: { start: createTime("00:00"), end: createTime("23:59") }, hub_id: hq._id }],
+            thursday:   [{ time: { start: createTime("00:00"), end: createTime("23:59") }, hub_id: hq._id }],
+            friday:     [{ time: { start: createTime("00:00"), end: createTime("23:59") }, hub_id: hq._id }],
+            saturday:   [{ time: { start: createTime("00:00"), end: createTime("23:59") }, hub_id: hq._id }],
+            sunday:     [{ time: { start: createTime("00:00"), end: createTime("23:59") }, hub_id: hq._id }]
+        }
     }).save().catch((err) => { console.log(err.message); });
 
     await mongoose.connection.close();
--- middleware/user.js
+++ middleware/user.js
@@ -11,60 +11,100 @@ function _errorHandler(req, res, next) {
     }
 }
 
+const validateWorkingTimeRequired = function() {
+    let out = [ validator.body("working_time").exists() ];
+
+    for (const week of ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]) {
+        out.push( validator.body(`working_time.${week}`).exists() );
+        out.push( validator.body(`working_time.${week}.*.time.start`).optional().isISO8601().toDate() );
+        out.push( validator.body(`working_time.${week}.*.time.end`).optional().isISO8601().toDate() );
+        out.push( validator.body(`working_time.${week}.*.hub_id`).optional().isMongoId() );
+    }
+
+    return out;
+}();
+
+const validateWorkingTimeOptional = function () {
+    let out = [validator.body("working_time").optional()];
+
+    for (const week of ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]) {
+        out.push( validator.body(`working_time.${week}`).optional() );
+        out.push( validator.body(`working_time.${week}.*.time`).optional() );
+        out.push( validator.body(`working_time.${week}.*.time.start`).optional().isISO8601().toDate() );
+        out.push( validator.body(`working_time.${week}.*.time.end`).optional().isISO8601().toDate() );
+        out.push( validator.body(`working_time.${week}.*.hub_id`).optional().isMongoId() );
+    }
+
+    return out;
+}();
+
+const validateAbsenceTime = function () {
+    let out = [validator.body("absence_time").optional()];
+
+    for (const week of ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]) {
+        out.push( validator.body(`working_time.${week}`).optional() );
+        out.push( validator.body(`working_time.${week}.*.time`).optional() );
+        out.push( validator.body(`working_time.${week}.*.time.start`).optional().isISO8601().toDate() );
+        out.push( validator.body(`working_time.${week}.*.time.end`).optional().isISO8601().toDate() );
+    }
+
+    return out;
+}();
+
 const validateInsertCustomer = [
-    validator.body("username").exists().trim(),
+    validator.body("username").exists().trim().escape(),
     validator.body("password").exists().isStrongPassword(),
     validator.body("email").exists().isEmail().normalizeEmail(),
-    validator.body("name").exists().trim(),
-    validator.body("surname").exists().trim(),
+    validator.body("name").exists().trim().escape(),
+    validator.body("surname").exists().trim().escape(),
     validator.body("gender").optional().trim().isIn(["M", "F", "Non-binary", "Altro"]),
-    validator.body("address.city").optional().trim(),
-    validator.body("address.street").optional().trim(),
-    validator.body("address.number").optional().trim(),
+    validator.body("address.city").optional().trim().escape(),
+    validator.body("address.street").optional().trim().escape(),
+    validator.body("address.number").optional().trim().escape(),
     validator.body("address.postal_code").optional().isPostalCode("any"),
     validator.body("phone").optional().isMobilePhone("any"),
     _errorHandler
 ];
 
 const validateInsertOperator = [
-    validator.body("username").exists().trim(),
+    validator.body("username").exists().trim().escape(),
     validator.body("password").exists().isStrongPassword(),
     validator.body("email").exists().isEmail().normalizeEmail(),
-    validator.body("name").exists().trim(),
-    validator.body("surname").exists().trim(),
+    validator.body("name").exists().trim().escape(),
+    validator.body("surname").exists().trim().escape(),
     validator.body("gender").optional().trim().isIn(["M", "F", "Non-binary", "Altro"]),
     validator.body("role_id").optional().isMongoId(),
-    // validator.body("permission"),
-    // validator.body("working_time"),
+    validator.body("permission").optional(),
+    validateWorkingTimeRequired,
     _errorHandler
 ];
 
 const validateSearchUser = [
-    validator.param("username").exists().trim(),
+    validator.param("username").exists().trim().escape(),
     _errorHandler
 ];
 
 const validateUpdateUser = [
-    validator.param("username").exists().trim(),
+    validator.param("username").exists().trim().escape(),
     validator.body("password").optional().isStrongPassword(),
     validator.body("email").optional().isEmail().normalizeEmail(),
-    validator.body("name").optional().trim(),
-    validator.body("surname").optional().trim(),
+    validator.body("name").optional().trim().escape(),
+    validator.body("surname").optional().trim().escape(),
     validator.body("gender").optional().trim().isIn(["M", "F", "Non-binary", "Altro"]),
-    validator.body("address.city").optional().trim(),
-    validator.body("address.street").optional().trim(),
-    validator.body("address.number").optional().trim(),
+    validator.body("address.city").optional().trim().escape(),
+    validator.body("address.street").optional().trim().escape(),
+    validator.body("address.number").optional().trim().escape(),
     validator.body("address.postal_code").optional().isPostalCode("any"),
     validator.body("phone").optional().isMobilePhone("any"),
     validator.body("role_id").optional().isMongoId(),
-    // validator.body("permission"),
-    // validator.body("working_time"),
-    // validator.body("absence_time"),
+    validator.body("permission").optional(),
+    validateWorkingTimeOptional,
+    validateAbsenceTime,
     _errorHandler
 ];
 
 const validateDeleteUser = [
-    validator.param("username").exists().trim(),
+    validator.param("username").exists().trim().escape(),
     _errorHandler
 ];
 
