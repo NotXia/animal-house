26ab27e0c636f83b36a3e26bdd24f7cffc9b50a7
NotXia <35894453+NotXia@users.noreply.github.com>
2022-05-31 11:54:54 +0200

    Update controller

--- controllers/shop/item.js
+++ controllers/shop/item.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const mongoose = require("mongoose");
 const ItemModel = require("../../models/shop/item");
-const ProductModel = require("../../models/shop/product");
 
 const { nanoid } = require("nanoid");
 const validator = require("express-validator");
@@ -14,42 +13,22 @@ const utils = require("../../utilities");
     Gestisce la creazione di un nuovo item comprensivo di prodotti 
 */
 async function createItem(req, res) {
-    let new_item_id;
-    const session = await mongoose.startSession();
+    const to_insert_item = validator.matchedData(req);
+    let new_item;
 
-    session.startTransaction();
     try {
-        const to_insert_item = validator.matchedData(req);
-        const to_insert_products = to_insert_item.products;
-
-        // Inserimento dei singoli prodotti
-        const products = await ProductModel.insertMany(to_insert_products);
-        const products_id = products.map((product) => product._id);
-    
-        // Composizione dell'item da inserire
-        delete to_insert_item.products;
-        to_insert_item.products_id = products_id;
-        
-        // Inserimento dell'item
-        const new_item = await new ItemModel(to_insert_item).save();
-        new_item_id = new_item.id;
-
-        await session.commitTransaction();
-        await session.endSession();
+        new_item = await new ItemModel(to_insert_item).save();
     }
     catch (err) {
-        await session.abortTransaction();
-        await session.endSession();
-
         switch (err.code) {
             case utils.MONGO_DUPLICATED_KEY:
-                return res.status(409).send({ field: "barcode", message: "Il prodotto associato al barcode è già presente in un item" });
+                return res.status(409).send({ field: "barcode", message: "Un barcode associato ad un prodotto è già presente in un item" });
             default:
                 return res.sendStatus(500);
         }
     }
 
-    return res.status(201).location(`${req.baseUrl}/items/${new_item_id}`).send({ id: new_item_id });
+    return res.status(201).location(`${req.baseUrl}/items/${new_item.id}`).send({ id: new_item.id });
 }
 
 /*
@@ -71,12 +50,6 @@ async function searchItem(req, res) {
 
     const items = await ItemModel.aggregate([
         { $match: query_criteria },
-        { $lookup: {
-            from: ProductModel.collection.name,
-            localField: "products_id",
-            foreignField: "_id",
-            as: "products"
-        }},
         { $project: {
             name: 1,
             description: 1,
@@ -103,36 +76,24 @@ async function searchItem(req, res) {
     Gestisce la ricerca di item usando il barcode di uno dei prodotti associati
 */
 async function searchItemByBarcode(req, res) {
-    let item = undefined; // Conterrà il risultato della ricerca
-
-    try {
-        // Cerca il prodotto associato al barcode e poi l'item che contiene il prodotto
-        const product = await ProductModel.findOne({ barcode: req.params.barcode }).exec();
-        if (product) {
-            item = await ItemModel.findOne({ products_id: product._id }).exec();
-        }
-    }
-    catch(err) {
+    const item = await ItemModel.findOne({ "products.barcode": req.params.barcode }).exec().catch(function (err) {
         return res.sendStatus(500); 
-    }
+    });
     
     if (!item) { return res.sendStatus(404); }
     return res.status(200).send(item);
 }
 
 /*
-    Gestisce la ricerca di tutti i prodotti associati ad un item
+    Gestisce l'estrazione di tutti i prodotti associati ad un item
 */
 async function searchProducts(req, res) {
-    let products = []; // Conterrà il risultato della ricerca
-
-    const item = await ItemModel.findById(req.params.item_id).populate("products_id", "-__v").exec().catch(function (err) {
+    const item = await ItemModel.findById(req.params.item_id).exec().catch(function (err) {
         return res.sendStatus(500);
     });
     if (!item) { return res.sendStatus(404); }
-    products = item.products_id;
-
-    return res.status(200).send(products);
+    
+    return res.status(200).send(item.products);
 }
 
 /*
@@ -154,20 +115,27 @@ async function updateItemById(req, res) {
 */
 async function updateProductByIndex(req, res) {
     const updated_fields = validator.matchedData(req, { locations: ["body"] });
-    let updated_product;
 
-    // Estrazione del prodotto a partire dall'item
-    try {
-        const item = await ItemModel.findById(req.params.item_id, { "products_id": 1 }).exec();
-        if (!item || !item.products_id[parseInt(req.params.product_index)]) { return res.sendStatus(404); }
-    
-        updated_product = await ProductModel.findByIdAndUpdate(item.products_id[parseInt(req.params.product_index)], updated_fields, { new: true });
+    // Verifica esistenza ed estrazione prodotto  
+    const item = await ItemModel.findById(req.params.item_id, { products: 1 }).exec();
+    if (!item || !item.products[parseInt(req.params.product_index)]) { return res.sendStatus(404); }
+    let product = item.products[parseInt(req.params.product_index)];
+
+    // Aggiornamento prodotto localmente
+    for (const [key, value] of Object.entries(updated_fields)) {
+        product[key] = value;
     }
-    catch(err) {
+
+    // Aggiornamento nel database
+    const updated_item = await ItemModel.findByIdAndUpdate(
+        req.params.item_id, 
+        { [`products.${req.params.product_index}`]: product }, 
+        { new: true }
+    ).exec().catch(function (err) {
         return res.sendStatus(500);
-    }
+    });
 
-    return res.status(200).send(updated_product);
+    return res.status(200).send(updated_item.products[parseInt(req.params.product_index)]);
 }
 
 /* 
@@ -176,22 +144,21 @@ async function updateProductByIndex(req, res) {
 async function deleteItemById(req, res) {
     const session = await mongoose.startSession();
 
-    session.startTransaction();
     try {
+        session.startTransaction();
+        
         // Estrazione dei prodotti associati all'item
-        const to_delete_item = await ItemModel.findById(req.params.item_id, { products_id: 1 }).exec();
+        const to_delete_item = await ItemModel.findById(req.params.item_id, { products: 1 }).exec();
         if (!to_delete_item) { return res.sendStatus(404); }
 
         // Rimozione delle immagini associate ai prodotti
-        const products = await ProductModel.find({ _id: { $in: to_delete_item.products_id } }, { images_path: 1 }).exec();
-        for (const product of products) {
+        for (const product of to_delete_item.products) {
             for (const image of product.images_path) {
                 await fs.promises.rm(path.join(process.env.SHOP_IMAGES_DIR_ABS_PATH, image));
             }
         }
 
-        // Rimozione dei prodotti e dell'item
-        await ProductModel.deleteMany({ _id: { $in: to_delete_item.products_id } });
+        // Rimozione dell'item
         await ItemModel.findByIdAndDelete(req.params.item_id);
 
         await session.commitTransaction();
@@ -216,22 +183,21 @@ async function deleteProductByIndex(req, res) {
     try {
         session.startTransaction();
 
-        // Estrazione del prodotto
-        const item = await ItemModel.findById(req.params.item_id, { products_id: 1 }).exec();
-        if (!item || !item.products_id[parseInt(req.params.product_index)]) { return res.sendStatus(404); }
-        if (item.products_id.length === 1) { 
+        // Estrazione del prodotto e verifica esistenza
+        const item = await ItemModel.findById(req.params.item_id, { products: 1 }).exec();
+        if (!item || !item.products[parseInt(req.params.product_index)]) { return res.sendStatus(404); }
+        if (item.products.length === 1) { 
             return res.status(303).location(`${req.baseUrl}/items/${req.params.item_id}`).send({message: "Per rimuovere questo prodotto bisogna rimuovere l'item"}) 
         }
 
-        // Rimozione delle immagini associate al prodotti
-        const to_delete_product = await ProductModel.findById(item.products_id[parseInt(req.params.product_index)], { images_path: 1 }).exec();
-        for (const image of to_delete_product.images_path) {
+        // Rimozione delle immagini associate al prodotto
+        for (const image of item.products[parseInt(req.params.product_index)].images_path) {
             await fs.promises.rm(path.join(process.env.SHOP_IMAGES_DIR_ABS_PATH, image));
         }
 
-        // Rimozione dei prodotti e dell'item
-        await ProductModel.findByIdAndDelete(to_delete_product._id);
-        await ItemModel.findByIdAndUpdate(req.params.item_id, { $pull: { products_id: to_delete_product._id } });
+        // Rimozione del prodotto (workaround per eliminare un elemento per indice)
+        await ItemModel.findByIdAndUpdate(req.params.item_id, { $unset: { [`products.${req.params.product_index}`]: 1 } });
+        await ItemModel.findByIdAndUpdate(req.params.item_id, { $pull: { products: null } });
 
         await session.commitTransaction();
         await session.endSession();
@@ -252,15 +218,13 @@ async function deleteProductByIndex(req, res) {
 */
 async function createUploadImages(req, res) {
     try {
-        // Ricerca dell'id del prodotto
-        const item = await ItemModel.findById(req.params.item_id, { products_id: 1 }).exec();
-        if (!item) { return res.sendStatus(404); }
-        const product_id = item.products_id[parseInt(req.params.product_index)];
-        if (!product_id) { return res.sendStatus(404); }
+        // Verifica esistenza
+        const item = await ItemModel.findById(req.params.item_id, { products: 1 }).exec();
+        if (!item || !item.products[parseInt(req.params.product_index)]) { return res.sendStatus(404); }
 
-        // Salvataggio dei file nel filesystem
+        // Salvataggio dei file nel file system
         let files_name = []
-        for (const [key, file] of Object.entries(req.files)) {
+        for (const [_, file] of Object.entries(req.files)) {
             const filename = `${nanoid(process.env.IMAGES_NAME_LENGTH)}${path.extname(file.name)}`;
             files_name.push(filename);
 
@@ -268,7 +232,7 @@ async function createUploadImages(req, res) {
         }
 
         // Salvataggio dei nomi dei file nel database
-        await ProductModel.findByIdAndUpdate(product_id, { $push: { images_path: { $each: files_name } } });
+        await ItemModel.findByIdAndUpdate(req.params.item_id, { $push: { [`products.${req.params.product_index}.images_path`]: { $each: files_name } } });
     }
     catch (err) {
         return res.sendStatus(500);
@@ -286,18 +250,18 @@ async function deleteImageByIndex(req, res) {
     try {
         session.startTransaction();
 
-        // Estrazione del prodotto
-        const item = await ItemModel.findById(req.params.item_id, { products_id: 1 }).exec();
-        if (!item || !item.products_id[parseInt(req.params.product_index)]) { return res.sendStatus(404); }
-        const product = await ProductModel.findById(item.products_id[parseInt(req.params.product_index)], { images_path: 1 }).exec();
-        if (!product || !product.images_path[parseInt(req.params.image_index)]) { return res.sendStatus(404); }
+        // Verifica esistenza ed estrazione del prodotto
+        const item = await ItemModel.findById(req.params.item_id, { products: 1 }).exec();
+        if (!item || !item.products[parseInt(req.params.product_index)]) { return res.sendStatus(404); }
+        const product = item.products[parseInt(req.params.product_index)];
 
         // Determina l'immagine da cancellare
         const to_delete_image = product.images_path[parseInt(req.params.image_index)];
 
-        // Cancella l'immagine dal filesystem e database
+        // Cancella l'immagine dal file system e database
         await fs.promises.rm(path.join(process.env.SHOP_IMAGES_DIR_ABS_PATH, to_delete_image));
-        await ProductModel.findByIdAndUpdate(product._id, { $pull: { images_path: to_delete_image } });
+        await ItemModel.findByIdAndUpdate(req.params.item_id, { $unset: { [`products.${req.params.product_index}.images_path.${req.params.image_index}`]: 1 } });
+        await ItemModel.findByIdAndUpdate(req.params.item_id, { $pull: { [`products.${req.params.product_index}.images_path`]: null } });
 
         await session.commitTransaction();
         session.endSession();
