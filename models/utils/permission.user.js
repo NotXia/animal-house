f7b3c8e67130e088270990827fee6acbcf19b653
Zaid1710 <zaid1234@hotmail.it>
2022-06-03 15:57:22 +0200

    Add permessi

--- models/utils/permission.user.js
+++ models/utils/permission.user.js
@@ -5,7 +5,10 @@ const permissionSchema = mongoose.Schema({
     vip: { type: Boolean, default: false },
     post_write: { type: Boolean, default: true },
     post_read: { type: Boolean, default: true },
-    comment_write: { type: Boolean, default: true }
+    post_modify: { type: Boolean, default: true },
+    comment_write: { type: Boolean, default: true },
+    comment_read: { type: Boolean, default: true },
+    comment_modify: { type: Boolean, default: true },
 }, { _id: false });
 
 module.exports = permissionSchema;
