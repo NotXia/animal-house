45fa6c375980b76c69d8e9307f2e31640a029d61
Zaid1710 <zaid1234@hotmail.it>
2022-06-03 15:41:11 +0200

    Add permessi in permission.user

--- models/utils/permission.user.js
+++ models/utils/permission.user.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const permissionSchema = mongoose.Schema({
     user: { type: Boolean, default: true },
     vip: { type: Boolean, default: false },
-    write_post: { type: Boolean, default: true },
-    comment: { type: Boolean, default: true },
+    post_write: { type: Boolean, default: true },
+    post_read: { type: Boolean, default: true },
+    comment_write: { type: Boolean, default: true }
 }, { _id: false });
 
 module.exports = permissionSchema;
