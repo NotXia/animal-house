af8ab43a243c467fbd62fa2a58652adc00ee5a24
Zaid1710 <zaid1234@hotmail.it>
2022-06-03 15:41:35 +0200

    Correzioni nel router post.js

--- routes/post.js
+++ routes/post.js
@@ -6,25 +6,29 @@ const post_controller = require("../controllers/post");
 
 // Router per i post
 
-router.post("/posts/", [ auth_middleware([ ["write_post"], ["user"], ["admin"] ]), post_middleware.validateInsertPost ], post_controller.insertPost);
+router.post("/posts/", [ auth_middleware([ ["post_write"], ["admin"] ]), post_middleware.validateInsertPost ], post_controller.insertPost);
 
-router.get("/posts/users/:user_id", [ auth_middleware([ ["user"], ["admin"] ]), post_middleware.validateSearchPostByUser ], post_controller.searchPostByUser);
-router.get("/posts/:post_id", [ auth_middleware([ ["user"], ["admin"] ]), post_middleware.validateSearchPostById ], post_controller.searchPostById);
-router.get("/posts/category/:category", [ auth_middleware([ ["user"], ["admin"] ]), post_middleware.validateSearchPostByCategory ], post_controller.searchPostByCategory);
+router.get("/posts/users/:user_id", [ auth_middleware([ ["post_read"], ["admin"] ]), post_middleware.validateSearchPostByUser ], post_controller.searchPostByUser);
+router.get("/posts/:post_id", [ auth_middleware([ ["post_read"], ["admin"] ]), post_middleware.validateSearchPostById ], post_controller.searchPostById);
+router.get(
+    "/posts/category/:category", 
+    [ auth_middleware([ ["post_read"], ["admin"] ]), post_middleware.validateSearchPostByCategory ], 
+    post_controller.searchPostByCategory
+);
 
-router.put("/posts/:post_id", [ auth_middleware([ ["write_post"], ["user"], ["admin"] ]), post_middleware.validateUpdatePost ], post_controller.updatePost);
+router.put("/posts/:post_id", [ auth_middleware([ ["post_write"], ["admin"] ]), post_middleware.validateUpdatePost ], post_controller.updatePost);
 
-router.delete("/customers/:post_id", [ auth_middleware([ ["user"], ["admin"] ]), user_middleware.validateDeletePost ], post_controller.deletePost);
+router.delete("/customers/:post_id", [ auth_middleware([ ["post_write"], ["admin"] ]), post_middleware.validateDeletePost ], post_controller.deletePost);
 
 // Router per i commenti 
 
-router.post("/posts/:post_id/comments/", [ auth_middleware([ ["write_post"], ["user"], ["admin"] ]), post_middleware.validateInsertComment ], post_controller.insertComment);
+router.post("/posts/:post_id/comments/", [ auth_middleware([ ["comment_write"], ["admin"] ]), post_middleware.validateInsertComment ], post_controller.insertComment);
 
 router.get("/posts/:post_id/comments/", [ auth_middleware([ ["user"], ["admin"] ]), post_middleware.validateSearchCommentByPost ], post_controller.searchCommentByPost);
 router.get("/posts/:post_id/comments/:comment_index", [ auth_middleware([ ["user"], ["admin"] ]), post_middleware.validateSearchCommentByIndex ], post_controller.searchCommentByIndex);
 
-router.put("/posts/:post_id/comments/:comment_index", [ auth_middleware([ ["write_post"], ["user"], ["admin"] ]), post_middleware.validateUpdateComment ], post_controller.updateComment);
+router.put("/posts/:post_id/comments/:comment_index", [ auth_middleware([ ["comment_write"], ["admin"] ]), post_middleware.validateUpdateComment ], post_controller.updateComment);
 
-router.delete("/customers/:post_id/:comment_index", [ auth_middleware([ ["user"], ["admin"] ]), user_middleware.validateDeleteComment ], post_controller.deleteComment);
+router.delete("/customers/:post_id/:comment_index", [ auth_middleware([ ["comment_write"], ["admin"] ]), post_middleware.validateDeleteComment ], post_controller.deleteComment);
 
 module.exports = router;
