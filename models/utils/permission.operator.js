a7f764a68c700beda1855f29ea0e1dc166ee2b19
Zaid1710 <zaid1234@hotmail.it>
2022-06-03 18:56:31 +0200

    Add permessi operatore

--- models/utils/permission.operator.js
+++ models/utils/permission.operator.js
@@ -6,6 +6,13 @@ const permissionSchema = mongoose.Schema({
     
     shop_read: { type: Boolean, default: false },
     shop_write: { type: Boolean, default: false },
+
+    post_write: { type: Boolean, default: true },
+    post_read: { type: Boolean, default: true },
+    post_modify: { type: Boolean, default: true },
+    comment_write: { type: Boolean, default: true },
+    comment_read: { type: Boolean, default: true },
+    comment_modify: { type: Boolean, default: true },
 }, { _id: false });
 
 module.exports = permissionSchema;
