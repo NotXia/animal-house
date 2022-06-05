b8733c32a013c9fdde5cf52f393a515657061a10
Zaid1710 <zaid1234@hotmail.it>
2022-06-05 22:31:16 +0200

    Update permessi

--- models/utils/permission.operator.js
+++ models/utils/permission.operator.js
@@ -8,11 +8,7 @@ const permissionSchema = mongoose.Schema({
     shop_write: { type: Boolean, default: false },
 
     post_write: { type: Boolean, default: true },
-    post_read: { type: Boolean, default: true },
-    post_modify: { type: Boolean, default: true },
     comment_write: { type: Boolean, default: true },
-    comment_read: { type: Boolean, default: true },
-    comment_modify: { type: Boolean, default: true },
 }, { _id: false });
 
 module.exports = permissionSchema;
--- models/utils/permission.user.js
+++ models/utils/permission.user.js
@@ -4,11 +4,7 @@ const permissionSchema = mongoose.Schema({
     user: { type: Boolean, default: true },
     vip: { type: Boolean, default: false },
     post_write: { type: Boolean, default: true },
-    post_read: { type: Boolean, default: true },
-    post_modify: { type: Boolean, default: true },
     comment_write: { type: Boolean, default: true },
-    comment_read: { type: Boolean, default: true },
-    comment_modify: { type: Boolean, default: true },
 }, { _id: false });
 
 module.exports = permissionSchema;
