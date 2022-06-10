db59be02af4d2156cf461f748804c8a208606f62
NotXia <35894453+NotXia@users.noreply.github.com>
2022-06-10 17:42:52 +0200

    Update modelli per gestire utenti

--- models/auth/customer.js
+++ models/auth/customer.js
@@ -1,48 +1,14 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const addressSchema = require("../utils/address");
-const permissionSchema = require("../utils/permission.user");
 const AnimalModel = require("../animals/animal");
 const ProductModel = require("../shop/product");
 
 const customerScheme = mongoose.Schema({
-    username: { 
-        type: String, 
-        required: true, 
-        unique: true 
-    },
-    password: { 
-        type: String, 
-        required: true 
-    },
-    email: { 
-        type: String, 
-        required: true,
-        unique: true,
-        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    },
-
-    name: { type: String, required: true }, 
-    surname: { type: String, required: true },
-    gender: { type: String },
-
     address: [{ // [0] contiene l'indirizzo principale
         type: addressSchema,
     }],
-    phone: { type: String },
 
-    enabled: { 
-        type: Boolean, 
-        required: true, 
-        default: false 
-    },
-    creationDate: { 
-        type: Date, 
-        required: true, 
-        default: new Date() 
-    },
-
-    permission: { type: permissionSchema, default: {} }, // Eredita i campi default dello Schema
     animals_id: [{ type: ObjectId, ref: AnimalModel.collection.collectionName }],
 
     cart: [{ type: ObjectId, ref: ProductModel.collection.collectionName }]
--- models/auth/operator.js
+++ models/auth/operator.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const timeSlotSchema = require("../utils/timeSlotSchema");
 const getAgendaSchema = require("../utils/agenda");
-const permissionSchema = require("../utils/permission.operator");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const HubModel = require("../services/hub");
 const RoleModel = require("../services/role");
@@ -18,46 +17,11 @@ const workingSlot = mongoose.Schema({
 }, { _id: false });
 
 const operatorScheme = mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    },
-
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
-    gender: { type: String },
-    phone: { type: String },
-
-    enabled: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    creationDate: {
-        type: Date,
-        required: true,
-        default: new Date()
-    },
-
     role_id: { 
         type: ObjectId, ref: RoleModel.collection.collectionName, 
         required: true,
     },
-    permission: {
-        type: permissionSchema,
-        default: {} // Eredita i campi default dello Schema
-    },
+
     working_time: {
         type: getAgendaSchema(workingSlot),
         required: true,
