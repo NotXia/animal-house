d463421e89255339d307749c0b05c067f1ce04a4
NotXia <35894453+NotXia@users.noreply.github.com>
2022-06-19 20:11:58 +0200

    Update gestione codici HTTP

--- error_handler.js
+++ error_handler.js
@@ -1,10 +1,34 @@
+const { http } = require("./utilities");
+
+function defaultErrorMessage(code) {
+    switch (code) {
+        case http.BAD_REQUEST:
+            return "Richiesta malformata";
+        case http.UNAUTHORIZED:
+            return "Non autorizzato";
+        case http.FORBIDDEN:
+            return "Permessi insufficienti"
+        case http.NOT_FOUND:
+            return "Risorsa non trovata";
+        case http.CONFLICT:
+            return "Risorsa già esistente";
+        case http.INTERNAL_SERVER_ERROR:
+            return "Errore interno";
+        default:
+            return "";
+    }
+}
+
 /**
  * Crea un formato adatto per un messaggio di errore.
+ * Se il messaggio è un numero, si assume che sia un codice HTTP e lo si sostituisce con il corrispondente messaggio di default.
  * Se il messaggio è una stringa pura, viene inglobato in un object, altrimenti rimane inalterato.
  * @param {string} err_message 
  * @returns Messaggio formattato
  */
 function formatErrorMessage(err_message) {
+    if (typeof err_message === "number") { err_message = defaultErrorMessage(err_message); }
+
     if (typeof err_message === "string") {
         err_message = { message: err_message };
     }
@@ -12,35 +36,33 @@ function formatErrorMessage(err_message) {
     return JSON.stringify(err_message);
 }
 
-const error_generator = {
-    BAD_REQUEST: function (message = "Richiesta malformata") {
-        let err = new Error(formatErrorMessage(message)); err.code = 400;
+function errorGenerator(code) {
+    return function(message="") {
+        if (message.length === 0) { message = defaultErrorMessage(code); }
+        let err = new Error(formatErrorMessage(message)); 
+        err.code = code;
         return err;
-    },
-    UNAUTHORIZED: function (message = "Non autorizzato") { // Non autenticato
-        let err = new Error(formatErrorMessage(message)); err.code = 401;
-        return err;
-    },
-    FORBIDDEN: function (message = "Permessi mancanti") { // Autenticato ma senza permessi
-        let err = new Error(formatErrorMessage(message)); err.code = 403;
-        return err;
-    },
-    NOT_FOUND: function (message = "Non trovato") {
-        let err = new Error(formatErrorMessage(message)); err.code = 404;
-        return err;
-    }
+    };
+}
+
+const errors = {
+    BAD_REQUEST:    errorGenerator(http.BAD_REQUEST),
+    UNAUTHORIZED:   errorGenerator(http.UNAUTHORIZED),
+    FORBIDDEN:      errorGenerator(http.FORBIDDEN),
+    NOT_FOUND:      errorGenerator(http.NOT_FOUND)
 }
 
 function errorHandler(err, req, res, next) {
     if (err) {
         switch (err.code) {
-            case 400:
-            case 401:
-            case 403:
-            case 404:
+            case http.BAD_REQUEST:
+            case http.UNAUTHORIZED:
+            case http.FORBIDDEN:
+            case http.NOT_FOUND:
+            case http.CONFLICT:
                 return res.status(err.code).send(JSON.parse(err.message));
             default:
-                return res.status(500).send();
+                return res.status(http.INTERNAL_SERVER_ERROR).send();
         }
     }
     else {
@@ -50,6 +72,7 @@ function errorHandler(err, req, res, next) {
 
 module.exports = {
     errorHandler: errorHandler,
-    generate: error_generator,
-    formatMessage: formatErrorMessage
+    generate: errors,
+    formatMessage: formatErrorMessage,
+    defaultMessage: defaultErrorMessage
 };
--- utilities.js
+++ utilities.js
@@ -13,9 +13,21 @@ function createTime(time) {
     return new Date(`${base_date} ${time}`);
 }
 
+const http_code = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    INTERNAL_SERVER_ERROR: 500
+}
 
 module.exports = {
     createTime: createTime,
+
+    http: http_code,
 
     // Variabili utili
     MONGO_DUPLICATED_KEY: 11000,
