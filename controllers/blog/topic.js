20c3eac6479258a58c2b6b14d5ef69a101c48511
NotXia <35894453+NotXia@users.noreply.github.com>
2022-07-03 18:55:55 +0200

    Add GET topics

--- controllers/blog/topic.js
+++ controllers/blog/topic.js
@@ -3,13 +3,17 @@ const mongoose = require("mongoose");
 const utils = require("../../utilities");
 const error = require("../../error_handler");
 const TopicModel = require("../../models/blog/topic");
+const validator = require('express-validator');
 
 async function insertTopic(req, res) {
-
+    
 }
 
 async function getTopics(req, res) {
-    
+    const topics = await TopicModel.find({}, { _id: 0 }).exec().catch(function (err) { 
+        return res.status(utils.http.INTERNAL_SERVER_ERROR).json(error.formatMessage(utils.http.INTERNAL_SERVER_ERROR));
+    });
+    return res.status(utils.http.OK).json(topics);
 }
 
 async function updateTopic(req, res) {
