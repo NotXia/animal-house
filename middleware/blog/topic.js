42a4c33339a1845e99cd7bedd2de4edc3b8c89f5
NotXia <35894453+NotXia@users.noreply.github.com>
2022-07-03 19:47:16 +0200

    Update middleware

--- middleware/blog/topic.js
+++ middleware/blog/topic.js
@@ -1,24 +1,32 @@
 const validator = require('express-validator');
 const utils = require("../utils");
 
-function validateName(source) { return source("name").trim().escape(); }
-function validateIcon(source) { return source("icon").isBase64().withMessage("Formato non valido"); }
+function validateName(source, required=false, field_name="name") {
+    let validator = source(field_name);
+    if (required) { validator.exists().withMessage("Valore mancante"); } else { validator.optional(); }
+    return validator.trim().escape();
+}
+function validateIcon(source, required=false, field_name="icon") {
+    let validator = source(field_name);
+    if (required) { validator.exists().withMessage("Valore mancante"); } else { validator.optional(); }
+    return validator.isBase64().withMessage("Formato non valido");
+}
 
 const validateInsertTopic = [
-    validateName(validator.body).exists().withMessage("Valore mancante"),
-    validateIcon(validator.body).optional(),
+    validateName(validator.body, true),
+    validateIcon(validator.body, false),
     utils.validatorErrorHandler
 ];
 
 const validateUpdateTopic = [
-    validator.param("topic").exists().withMessage("Valore mancante").trim().escape(),
-    validateName(validator.body).optional(),
-    validateIcon(validator.body).optional(),
+    validateName(validator.param, true, "topic"),
+    validateName(validator.body, false),
+    validateIcon(validator.body, false),
     utils.validatorErrorHandler
 ];
 
 const validateDeleteTopic = [
-    validator.param("topic").exists().withMessage("Valore mancante").trim().escape(),
+    validateName(validator.param, true, "topic"),
     utils.validatorErrorHandler
 ];
 
