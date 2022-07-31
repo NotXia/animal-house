283777a3440948062ecae17a1a95ff2ca754dbfc
Zaid1710 <zaid1234@hotmail.it>
2022-07-31 12:11:42 +0200

    Fix controller

--- controllers/hub.js
+++ controllers/hub.js
@@ -3,7 +3,6 @@ const HubModel = require("../models/services/hub");
 const utils = require("../utilities");
 const error = require("../error_handler");
 const { matchedData } = require('express-validator');
-const hub = require('../middleware/hub');
 
 // Inserimento di un hub
 async function insertHub(req, res) {
@@ -13,11 +12,11 @@ async function insertHub(req, res) {
         return res.status(utils.http.CREATED)
             .location(`${req.baseUrl}/${toInsertHub.code}`)
             .json(toInsertHub.getData());
-    } catch (e) {
-        if (e.code === utils.MONGO_DUPLICATED_KEY) {
-            e = error.generate.CONFLICT({ field: "code", message: "Codice già in uso" });
+    } catch (err) {
+        if (err.code === utils.MONGO_DUPLICATED_KEY) {
+            err = error.generate.CONFLICT({ field: "code", message: "Codice già in uso" });
         }
-        return error.response(e, res);
+        return error.response(err, res);
     }
 }
 
@@ -66,10 +65,8 @@ async function updateHub(req, res) {
 // Cancellazione di un hub dato il codice
 async function deleteHub(req, res) {
     try {
-        const hub = await HubModel.findOne({ code: req.params.code }).exec();
-
-        const deletedHub = await HubModel.findOneAndDelete({ code: hub.code });
-        if (deletedHub.deletedCount === 0) { throw error.generate.NOT_FOUND("Hub inesistente"); }
+        const deletedHub = await HubModel.findOneAndDelete({ code: req.params.code });
+        if (!deletedHub) { throw error.generate.NOT_FOUND("Hub inesistente"); }
         
         return res.sendStatus(utils.http.NO_CONTENT);
     } catch (err) {
