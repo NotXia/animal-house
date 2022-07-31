21408c36da4314048a4980cb5b507904e3c981cf
Zaid1710 <zaid1234@hotmail.it>
2022-07-31 14:36:48 +0200

    Add controllo intervallo tempo

--- middleware/validators/hub.js
+++ middleware/validators/hub.js
@@ -2,8 +2,10 @@ const validator = require("express-validator");
 const utils = require("./utils");
 const userValidator = require("./user");
 const { WEEKS } = require("../../utilities");
+const moment = require("moment");
+const error = require("../../error_handler");
 
-module.exports.validateCode =       (source, required=true, field_name="code") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").trim().escape().matches(/^[A-Z]{3}[1-9][0-9]*$/); }
+module.exports.validateCode =       (source, required=true, field_name="code") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").trim().escape().matches(/^[A-Z]{3}[1-9][0-9]*$/).withMessage("Codice malformato"); }
 module.exports.validateName =       (source, required=true, field_name="name") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").trim().escape(); }
 module.exports.validateAddress =    (source, required=true, field_name="address") => { return userValidator.validateAddress(source, required, field_name); }
 module.exports.validateOpeningTime = function (source, required=true, field_name="opening_time") {
@@ -15,6 +17,24 @@ module.exports.validateOpeningTime = function (source, required=true, field_name
             out.push(validator[source](`${field_name}.${week}.*.start`).optional().isISO8601().withMessage("Formato non valido").toDate());
             out.push(validator[source](`${field_name}.${week}.*.end`).optional().isISO8601().withMessage("Formato non valido").toDate());
         }
+
+        out.push(
+            // Verifica validità intervalli temporali
+            function (req, res, next) {
+                if (!req[source][field_name]) {
+                    return next();
+                }
+                for (const week of WEEKS) {
+                    for (const opening_time of req[source][field_name][week]) {
+                        if (moment(opening_time.start) >= moment(opening_time.end)) { 
+                            next( error.generate.BAD_REQUEST([{ field: field_name, message: "Intervallo di tempo invalido" }]) );
+                        }
+                    }
+                }
+    
+                next();
+            }
+        )
 
         return out;
     }
