b2ef002de1833d485cbcf05da7d4fdb928bb8cc5
Zaid1710 <zaid1234@hotmail.it>
2022-07-31 14:36:29 +0200

    Add withMessage validatore user

--- middleware/validators/user.js
+++ middleware/validators/user.js
@@ -60,10 +60,10 @@ module.exports.validateAddress = function (source, required=true, field_name="ad
     if (required) {
         return [
             utils.handleRequired(validator[source](field_name), utils.REQUIRED),
-            validator[source](`${field_name}.city`).exists().trim().escape(),
-            validator[source](`${field_name}.street`).exists().trim().escape(),
-            validator[source](`${field_name}.number`).exists().trim().escape(),
-            validator[source](`${field_name}.postal_code`).exists().isPostalCode("any").withMessage("Formato non valido"),
+            validator[source](`${field_name}.city`).exists().withMessage("Valore mancante").trim().escape(),
+            validator[source](`${field_name}.street`).exists().withMessage("Valore mancante").trim().escape(),
+            validator[source](`${field_name}.number`).exists().withMessage("Valore mancante").trim().escape(),
+            validator[source](`${field_name}.postal_code`).exists().withMessage("Valore mancante").isPostalCode("any").withMessage("Formato non valido"),
         ];
     }
     else {
