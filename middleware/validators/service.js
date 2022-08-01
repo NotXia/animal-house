3c78cf23552fe2cdeba5ff2d9befe5d6cd1c5dce
Zaid1710 <zaid1234@hotmail.it>
2022-08-01 14:06:59 +0200

    Add validators

--- middleware/validators/service.js
+++ middleware/validators/service.js
@@ -1,4 +1,6 @@
 const validator = require("express-validator");
 const utils = require("./utils");
 
-module.exports.validateServiceName =   (source, required=true, field_name="name") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").trim().escape(); }
+module.exports.validateServiceName =            (source, required=true, field_name="name") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").trim().escape(); }
+module.exports.validateServiceDescription =     (source, required=true, field_name="description") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").trim().escape(); }
+module.exports.validateServiceDuration =        (source, required=true, field_name="duration") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").isInt({ min: 0 }); }
