479a1ebafd6aed028bb42e57892cfed185e2898a
Zaid <14925100+Zaid1710@users.noreply.github.com>
2022-08-01 19:15:36 +0200

    Update middleware/validators/service.js
    Co-authored-by: Tian Cheng Xia <35894453+NotXia@users.noreply.github.com>

--- middleware/validators/service.js
+++ middleware/validators/service.js
@@ -3,4 +3,4 @@ const utils = require("./utils");
 
 module.exports.validateServiceName =            (source, required=true, field_name="name") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").trim().escape(); }
 module.exports.validateServiceDescription =     (source, required=true, field_name="description") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").trim().escape(); }
-module.exports.validateServiceDuration =        (source, required=true, field_name="duration") => { return utils.handleRequired(validator[source](field_name), required).notEmpty().withMessage("Valore mancante").isInt({ min: 0 }); }
+module.exports.validateServiceDuration =        (source, required=true, field_name="duration") => { return utils.handleRequired(validator[source](field_name), required).isInt({ min: 0 }).withMessage("Valore invalido"); }
