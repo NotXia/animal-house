59904572f2120a9c79aba42f6aa0a5c729d6db24
Zaid1710 <zaid1234@hotmail.it>
2022-08-02 17:55:13 +0200

    Update test

--- tests/service.test.js
+++ tests/service.test.js
@@ -23,7 +23,8 @@ describe("Creazione di un servizio", function () {
             name: "Vaccino antirabbia",
             description: "Vaccino antirabbia per il vostro animale domestico",
             duration: 60,
-            price: 1000
+            price: 1000,
+            target: ["Cane", "Gatto"]
         }).set({ Authorization: `Bearer ${admin_token}` }).expect(201);
         expect(res.body).toBeDefined();
 
@@ -32,6 +33,8 @@ describe("Creazione di un servizio", function () {
         expect(service).toBeDefined();
         expect(service.name).toEqual("Vaccino antirabbia");
         expect(service.duration).toEqual(60);
+        expect(service.target.length).toEqual(2);
+        expect(service.target[1]).toEqual("Gatto");
     });
 
     test("Creazione corretta", async function () {
@@ -86,7 +89,8 @@ describe("Modifica di servizi", function () {
     test("Modifica del nome", async function () {
         await curr_session.put(`/services/${servizioTest._id}`).send({
             name: "Fisioterapia",   // era Vaccino antirabbia
-            price: 3000             // era 1000
+            price: 3000,            // era 1000
+            target: ["Cane"]        // era ["Cane", "Gatto"]
         }).set({ Authorization: `Bearer ${admin_token}` }).expect(200);
 
         const service = await ServiceModel.findOne({ name: "Fisioterapia" }).exec();
@@ -95,6 +99,8 @@ describe("Modifica di servizi", function () {
         expect(service.name).toEqual("Fisioterapia");
         expect(service.duration).toEqual(60);
         expect(service.price).toEqual(3000);
+        expect(service.target.length).toEqual(1);
+        expect(service.target[0]).toEqual("Cane");
     });
 
     test("Modifica servizio inesistente", async function () {
