e1537888b52a959f853a8bced83a730fe4c0188b
NotXia <35894453+NotXia@users.noreply.github.com>
2022-08-07 10:00:13 +0200

    Update modello

--- models/services/booking.js
+++ models/services/booking.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 const timeSlotSchema = require("../utils/timeSlotSchema");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const ServiceModel = require("./service");
+const AnimalModel = require("../animals/animal");
 
 const bookingScheme = mongoose.Schema({
     time_slot: { type: timeSlotSchema, required: true },
     service_id: { type: ObjectId, ref: ServiceModel.collection.collectionName, required: true },
 
     customer: { type: String, required: true },
+    animal_id: { type: ObjectId, ref: AnimalModel.collection.collectionName, required: true },
     operator: { type: String, required: true },
     hub: { type: String, required: true }
 });
