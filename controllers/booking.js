4a1179243ce333a730514f3a61b7016399adbb86
NotXia <35894453+NotXia@users.noreply.github.com>
2022-08-07 10:41:22 +0200

    Add commenti

--- controllers/booking.js
+++ controllers/booking.js
@@ -5,6 +5,7 @@ const OperatorModel = require("../models/auth/operator");
 const ServiceModel = require("../models/services/service");
 const moment = require("moment");
 
+/* Ricerca degli slot disponibili */
 async function searchAvailabilities(req, res) {
     let availabilities = [];
     const hub_code = req.query.hub_code;
@@ -13,13 +14,15 @@ async function searchAvailabilities(req, res) {
     const end_date = req.query.end_date;
 
     try {
+        // Ricerca dati servizio
+        const service = await ServiceModel.findById(service_id).exec();
+
         // Ricerca operatori
         let operator_query = { "$or": [], services_id: service_id };
         for (const days of utils.WEEKS) { operator_query["$or"].push( { [`working_time.${days}.hub`]: hub_code } ); }
         const operators = await OperatorModel.find(operator_query).exec();
 
-        const service = await ServiceModel.findById(service_id).exec();
-
+        // Unione di tutti gli slot disponibili
         for (const operator of operators) {
             availabilities = availabilities.concat(await operator.getAvailabilityData(start_date, end_date, hub_code, service.duration));
         }
