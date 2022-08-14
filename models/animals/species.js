766e9b8c4fb5c7f9bcacc2e58a62e383464c070d
Zaid1710 <zaid1234@hotmail.it>
2022-08-14 15:53:20 +0200

    Update species model

--- models/animals/species.js
+++ models/animals/species.js
@@ -3,12 +3,17 @@ const mongoose = require("mongoose");
 const speciesSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     }
 });
 
 speciesSchema.statics.findByName = async function(species_name) {
     return await this.findOne({ name: species_name }).exec();
+};
+
+speciesSchema.methods.getData = function() {
+    return { name: this.name };
 };
 
 module.exports = mongoose.model("species", speciesSchema);
