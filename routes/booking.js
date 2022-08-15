47b655ca512ecb7884c1dd80d2dce69e16666353
Zaid1710 <zaid1234@hotmail.it>
2022-08-15 16:21:53 +0200

    Update router

--- routes/booking.js
+++ routes/booking.js
@@ -4,12 +4,12 @@ const auth_middleware = require("../middleware/auth");
 const booking_middleware = require("../middleware/booking");
 const booking_controller = require("../controllers/booking");
 
-router.post("/", [ auth_middleware([ ["customer"] ], [ ["admin"] ]), booking_middleware.validateInsertAppointment ], booking_controller.insertAppointment);
+router.post("/", [ auth_middleware([ ["customer"], ["operator"] ], [ ["admin"] ]), booking_middleware.validateInsertAppointment ], booking_controller.insertAppointment);
 
 router.get("/availabilities/", booking_middleware.validateSearchAvailabilities, booking_controller.searchAvailabilities);
 router.get("/:appointment_id", booking_middleware.validateGetAppointmentById, booking_controller.getAppointmentById);
 router.get("/", booking_middleware.validateGetAppointmentsByUser, booking_controller.getAppointmentsByUser);
 
-router.delete("/:appointment_id", [ auth_middleware([ ["customer"] ], [ ["admin"] ]), booking_middleware.validateDeleteAppointment ], booking_controller.deleteAppointment);
+router.delete("/:appointment_id", [ auth_middleware([ ["customer"], ["operator"] ], [ ["admin"] ]), booking_middleware.validateDeleteAppointment ], booking_controller.deleteAppointment);
 
 module.exports = router;
