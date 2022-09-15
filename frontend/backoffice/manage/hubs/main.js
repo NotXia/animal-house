60ffde943b432882de74afd1f80a11aac1bf6714
NotXia <35894453+NotXia@users.noreply.github.com>
2022-09-15 11:43:31 +0200

    Aggiornamento menù dopo modifica

--- frontend/backoffice/manage/hubs/main.js
+++ frontend/backoffice/manage/hubs/main.js
@@ -67,6 +67,7 @@ $(document).ready(async function() {
                         }
 
                         hub_cache[res_hub.code] = res_hub;
+                        updateHubMenu(res_hub);
                         showHub(hub_cache[res_hub.code]);
                     }
                     catch (err) {
@@ -163,12 +164,16 @@ function showHub(hub) {
 
 
 function addHubToMenu(hub) {
+    const code = he.encode(hub.code);
+    const name = he.encode(hub.name);
+    const address = `${he.encode(hub.address.street)} ${he.encode(hub.address.number)}, ${he.encode(hub.address.city)}`;
+
     $("#hub-menu-container").append(`
         <li class="nav-item mb-2">
             <button id="show-hub-${hub.code}" class="w-100 btn btn-outline-dark">
                 <div class="w-100 p-1 text-start">
-                    <p class="my-0"><span class="fw-bold">${he.encode(hub.code)}</span> ${he.encode(hub.name)}</p>
-                    <p class="my-0">${he.encode(hub.address.street)} ${he.encode(hub.address.number)}, ${he.encode(hub.address.city)}</p>
+                    <p class="my-0"><span class="fw-bold" id="menu-hub-code-${hub.code}">${code}</span> <span id="menu-hub-name-${hub.code}">${name}</span></p>
+                    <p class="my-0" id="menu-hub-address-${hub.code}">${address}</p>
                 </div>
             </button>
         </li>
@@ -177,4 +182,10 @@ function addHubToMenu(hub) {
     $(`#show-hub-${hub.code}`).on("click", function () {
         showHub(hub_cache[hub.code]);
     });
+}
+
+function updateHubMenu(hub) {
+    $(`#menu-hub-code-${hub.code}`).text(hub.code);
+    $(`#menu-hub-name-${hub.code}`).text(hub.name);
+    $(`#menu-hub-address-${hub.code}`).text(`${hub.address.street} ${hub.address.number}, ${hub.address.city}`);
 }
