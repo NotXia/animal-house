9e9de332a18e74f5c541e807a9b0fca2ad5a9b96
NotXia <35894453+NotXia@users.noreply.github.com>
2022-09-18 19:24:12 +0200

    Fix generazione specie

--- frontend/backoffice/manage/shop/items/view/species.js
+++ frontend/backoffice/manage/shop/items/view/species.js
@@ -7,7 +7,7 @@ export async function init() {
     for (const species of all_species) {
         $("#fieldset-target_species").append(`
             <div class="form-check form-check-inline">
-                <input type="checkbox" id="checkbox-target_species-${species.name}" class="form-check-input" value="${species.name}">
+                <input type="checkbox" id="checkbox-target_species-${species.name}" class="form-check-input" name="target_species" value="${species.name}">
                 <label class="form-check-label" for="checkbox-target_species-${species.name}">
                     <img src="data:image/*;base64,${species.logo}" alt="" class="ah-icon">
                     ${species.name}
