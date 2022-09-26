0d181daf9c3f947e84b5867434bf5e67c2275c07
NotXia <35894453+NotXia@users.noreply.github.com>
2022-09-26 12:35:40 +0200

    Cambiato gestione input testuale

--- frontend/frontoffice/src/components/form/TextInput/index.jsx
+++ frontend/frontoffice/src/components/form/TextInput/index.jsx
@@ -7,24 +7,35 @@
  *  - name          name dell'elemento <input>
  *  - type          type dell'elemento <input>
  *  - label         label da visualizzare
- *  - validation    funzione che valida il valore dell'input con interfaccia [function(value)] o [async function(value)]
+ *  - validation    funzione che valida il valore dell'input con interfaccia {{ [async] function(value, required) }}
  *  - required      required dell'elento <input>
+ * 
+ * Funzioni esposte:
+ *      async validate()     valida l'input
+ *      value()           restituisce il valore dell'input
+ *      focus()              mette il focus sull'input
  * 
  */
 
 import React from "react";
+import $ from "jquery";
 
-export default class GenericTextInput extends React.Component {
+export default class TextInput extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            value: "",
+
             error_message: "",
             valid: null,
             hadError: false
         };
 
-        this.inputValidation = this.inputValidation.bind(this);
         this.validation_delay;
+        this._inputValidation = this._inputValidation.bind(this);
+        this.validate = this.validate.bind(this);
+        this.value = this.value.bind(this);
+        this.focus = this.focus.bind(this);
     }
 
     render() {
@@ -39,7 +50,7 @@ export default class GenericTextInput extends React.Component {
                 <div className="form-floating w-100">
                     <input id={this.props.id} className={`form-control ${invalid_form_class}`}
                             type={this.props.type} required={required_attr}
-                            name={this.props.name} placeholder=" " onChange={(e) => this.inputValidation(e)}
+                            name={this.props.name} placeholder=" " onChange={(e) => this._inputValidation(e)}
                             aria-label={this.props.label} aria-invalid={aria_invalid} aria-errormessage={feedback_container_id} />
                     <label htmlFor={this.props.id} aria-hidden="true">{this.props.label}</label>
                 </div>
@@ -50,17 +61,41 @@ export default class GenericTextInput extends React.Component {
     }
 
 
-    async inputValidation(e) {
-        clearTimeout(this.validation_delay); // Annulla il timer precedente
+    async _inputValidation(e) {
+        this.setState({ value: e.target.value });
 
+        clearTimeout(this.validation_delay); // Annulla il timer precedente
         if (this.props.validation) {
             this.validation_delay = setTimeout((async function() {
-                const error = await this.props.validation(e.target.value); // Validazione
-                
-                if (error)  { this.setState({ error_message: error, valid: false, hadError: true }); }
-                else        { this.setState({ error_message: "", valid: true }); }
+                this.validate();
             }).bind(this), 200);
         }
+    }
+
+    /**
+     * Valida l'input attuale
+     */
+    async validate() {
+        if (this.props.validation) {
+            const error = await this.props.validation(this.state.value, this.props.required); // Validazione
+                
+            if (error)  { this.setState({ error_message: error, valid: false, hadError: true }); }
+            else        { this.setState({ error_message: "", valid: true }); }
+        }
+    }
+
+    /**
+     * Restituisce il valore dell'input attuale
+     */
+    value() {
+        return this.state.value;
+    }
+
+    /**
+     * Mette il focus sull'input
+     */
+    focus() {
+        $(`#${this.props.id}`).trigger("focus");
     }
 }
 
---
+++ frontend/frontoffice/src/utilities/validation/UserValidation.js
@@ -0,0 +1,70 @@
+/**
+ * Validatori dei dati dell'utente
+ */
+
+import $ from "jquery";
+
+export default class UserValidation {
+
+    static async username(value, required) {
+        if (required && value.trim().length === 0)                      { return `Username mancante`; }
+        else if (value.trim().length > 0 && value.trim().length <= 2)   { return `Username troppo corto`; }
+
+        const username_availability = await $.ajax({ method: "GET", url: `${process.env.REACT_APP_DOMAIN}/users/usernames/available/${encodeURIComponent(value)}` }).catch((err)=>{});
+        if (!username_availability.available) { return `Username già in uso`; }
+
+        return "";
+    }
+
+    static async password(value, required) {
+        const hasLowercase = function (password) { return password.toUpperCase() != password; }
+        const hasUppercase = function (password) { return password.toLowerCase() != password; }
+        const hasNumber = function (password) { return String(password).match( /[0-9]/ ); }
+        const hasSpecialCharacter = function (password) { return String(password).match( /\W/ ); }
+
+        if (required && value.length === 0)  { return `Password mancante`; }
+        else if (value.length < 8)                      { return `La password deve contenere almeno 8 caratteri`; }
+        else if (!hasLowercase(value))                  { return `La password deve contenere almeno un carattere minuscolo`; }
+        else if (!hasUppercase(value))                  { return `La password deve contenere almeno un carattere maiuscolo`; }
+        else if (!hasNumber(value))                     { return `La password deve contenere almeno una cifra`; }
+        else if (!hasSpecialCharacter(value))           { return `La password deve contenere almeno un carattere speciale`; }
+        
+        return "";
+    }
+
+    static async email(value, required) {
+        const isValidEmail = function (email) {
+            return String(email.toLowerCase()).match( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ );
+        }
+
+        if (required && value.trim().length === 0)   { return `Email mancante`; }
+        else if (!isValidEmail(value))               { return `Email non valida`; }
+
+        const email_availability = await $.ajax({ method: "GET", url: `${process.env.REACT_APP_DOMAIN}/users/emails/available/${encodeURIComponent(value)}` }).catch((err)=>{});
+        if (!email_availability.available) { return `Email già in uso`; }
+
+        return "";
+    }
+
+    static name(value, required) {
+        if (required && value.trim().length === 0) { return `Nome mancante`; }
+
+        return "";
+    }
+
+    static surname(value, required) {
+        if (required && value.trim().length === 0) { return `Cognome mancante`; }
+
+        return "";
+    }
+
+    async phone(value, required) {
+        const isValidPhone = function (phone) { return String(phone).match( /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/ ); }
+
+        if (required && value.length === 0)  { return `Telefono mancante`; }
+        else if (!isValidPhone(value))       { return `Telefono non valido`; }
+        
+        return "";
+    }
+
+}
