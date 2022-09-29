1aa8ad566e5ac63fbcd9deb2a39d4dd339164ad4
NotXia <35894453+NotXia@users.noreply.github.com>
2022-09-29 20:39:57 +0200

    Rimossi import inutili

--- frontend/frontoffice/src/pages/Logout/index.jsx
+++ frontend/frontoffice/src/pages/Logout/index.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import { logout } from "../../import/auth.js"
 
 export default class Logout extends React.Component {
