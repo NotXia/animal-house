54221bb4e8620109d01c0e262b4223ea18721d01
NotXia <35894453+NotXia@users.noreply.github.com>
2022-10-01 17:47:42 +0200

    Add componente per elemento item

---
+++ frontend/frontoffice/src/components/shop/ItemCard/index.jsx
@@ -0,0 +1,75 @@
+/**
+ * 
+ * Visualizza i dati di un item in una card
+ * Proprietà:
+ *  - item      Item da rappresentare
+ * 
+ */
+
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import css from "./card.module.css"
+import {centToPrice} from "../../../utilities/currency"
+import { Link } from "react-router-dom";
+
+export default class ItemCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.all_products = props.item.products;
+        this.available_products = props.item.products.filter((product) => product.quantity > 0);
+        this.available = this.available_products.length > 0;
+    }
+    
+    render() {
+        let available_class = this.available ? "" : css["card-text-unavailable"];
+
+        return (<>
+            <Link to={`/shop/item?id=${this.props.item.id}`} className={`${css["card-link"]}`}>
+                <div className={`${css["card-container"]}`}>
+                    <Container>
+                        <Row>
+                            <div className={`d-flex justify-content-center align-items-center ${css["card-image-container"]}`}>
+                                <img src={this.getItemImagePath()} alt="" className={`${css["card-image"]}`}/>
+                            </div>
+                        </Row>
+                        <Row className="mt-2">
+                            <p className={`fw-semibold text-center fs-6 m-0 text-decoration-underline`}>{this.available ? "" : "Non disponibile"}</p>
+                            <p className={`text-center fs-4 m-0 ${available_class}`}>{this.props.item.name}</p>
+                            <p className={`fw-semibold text-center fs-5 m-0 ${available_class}`}>{this.getItemPriceString()}</p>
+                        </Row>
+                    </Container>
+                </div>
+            </Link>
+        </>);
+    }
+
+    getItemImagePath() {
+        /**
+         * L'immagine visualizzata è la prima del primo prodotto.
+         * - Se ci sono prodotti non disponibili, vengono ignorati nel calcolo.
+         * - Se tutti i prodotti sono finiti, si considerano tutti.
+         */
+        let ref_product = this.available ? this.available_products[0] : this.all_products[0];
+
+        if (ref_product.images.length > 0) { return `${process.env.REACT_APP_DOMAIN}${ref_product.images[0].path}`; }
+        else                               { return `${process.env.REACT_APP_DOMAIN}/shop/images/default.png`; }
+    }
+
+    getItemPriceString() {
+        /**
+         * Il prezzo rappresentante è quello del prodotto di minor costo.
+         * - Se ci sono prodotti non disponibili, vengono ignorati nel calcolo.
+         * - Se tutti i prodotti sono finiti, restituisce stringa vuota.
+         */
+        if (!this.available) { return ""; }
+
+        let min_cost_product = this.available_products.reduce((prev, curr) => prev.price < curr.price ? prev : curr); // Estrae il prodotto di costo minore
+        const price = centToPrice(min_cost_product.price);
+
+        return this.available_products.length > 1 ? `A partire da ${price}€` : `${price}€`;
+    }
+}
+
