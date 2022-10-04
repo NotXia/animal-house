9c6f686f8dbfe1ec8455cf9f15cb40dfe4af5aae
NotXia <35894453+NotXia@users.noreply.github.com>
2022-10-04 10:11:54 +0200

    Update visualizzatore immagini

--- frontend/frontoffice/src/components/shop/ImagesViewer/index.jsx
+++ frontend/frontoffice/src/components/shop/ImagesViewer/index.jsx
@@ -7,8 +7,10 @@
  */
 
 import React from "react";
-import Tab from "bootstrap/js/src/tab";
+import css from "./image.module.css";
 import $ from "jquery";
+window.jQuery = window.$ = global.jQuery = $;
+const bootstrap = require("bootstrap");
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -20,31 +22,33 @@ export default class ImagesViewer extends React.Component {
         super(props);
 
         let images = this.props.images;
-        if (!images || images.length === 0) { images = [{ path: `${process.env.REACT_APP_DOMAIN}/shop/images/default.png`, description: "" }] } // Se non ci sono immagini
+        if (!images || images.length === 0) { images = [{ path: `/shop/images/default.png`, description: "" }] } // Se non ci sono immagini
         
         this.state = {
             images: images,
-            selected: "1"
+            selected: 0
         }
     }
     
     componentDidMount() {
-        new Tab(document.querySelector('#tab-product-images'))
+        new bootstrap.Tab(document.querySelector('#tab-product-images'));
     }
 
     render() {
         return (<>
             <Container fluid>
+                {/* Immagine visualizzata */}
                 <Row>
                     <Zoom>
-                        <div className="d-flex justify-content-center align-items-center p-2" style={{height: "25rem", width: "100%"}}>
-                                <img src={`${process.env.REACT_APP_DOMAIN}${this.getSelectedImage().path}`} alt={this.getSelectedImage().description} 
-                                    style={{maxHeight: "100%", maxWidth: "100%"}} />
+                        <div className={`d-flex justify-content-center align-items-center p-2 ${css["image-box"]}`}>
+                            <img src={`${process.env.REACT_APP_DOMAIN}${this.getSelectedImage().path}`} alt={this.getSelectedImage().description} 
+                                style={{maxHeight: "100%", maxWidth: "100%"}} />
                         </div>
                     </Zoom>
                 </Row>
 
-                <Row>
+                {/* Selettore immagine */}
+                <Row className="mt-2">
                     <div className="d-flex justify-content-center overflow-auto">
                         <ul id="tab-product-images" className="nav nav-pills flex-nowrap mw-100" role="tablist">
                             {
@@ -53,10 +57,10 @@ export default class ImagesViewer extends React.Component {
                                     let isActive = index === 0;
 
                                     return (
-                                        <li key={`tab-image-control-${image.path}`} className="nav-item" role="none">
+                                        <li key={`tab-image-control-${image.path}`} className="nav-item mx-1" role="none">
                                             <button type="button" role="tab" data-bs-toggle="pill" aria-selected={isActive}
-                                                    className={`btn btn-link ${active_class}`} style={{height: "6rem", width: "6rem"}}
-                                                    onClick={()=>this.setState({selected: index})} onMouseEnter={()=>this.setState({selected: index})} onFocus={()=>this.setState({selected: index})} >
+                                                    className={`${css["image-selector-button"]} ${active_class}`}
+                                                    onClick={() => this.changeImage(index)} onMouseEnter={() => this.changeImage(index)} onFocus={() => this.changeImage(index)} >
                                                 <img src={`${process.env.REACT_APP_DOMAIN}${image.path}`} alt="" style={{maxHeight: "100%", maxWidth: "100%"}} />
                                             </button>
                                         </li>
@@ -72,6 +76,12 @@ export default class ImagesViewer extends React.Component {
 
     getSelectedImage() {
         return this.state.images[this.state.selected];
+    }
+
+    changeImage(index) {
+        this.setState({selected: index});
+
+        $(`#tab-product-images li:nth-child(${index+1}) button`).tab("show");
     }
 }
 
