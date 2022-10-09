b7f9ad2520f6cc54044d630fb213ece572a0751d
NotXia <35894453+NotXia@users.noreply.github.com>
2022-10-09 13:01:38 +0200

    Fix API ordinamento

--- controllers/shop/item.js
+++ controllers/shop/item.js
@@ -91,10 +91,10 @@ async function searchItem(req, res) {
     if (req.query.name) { query_criteria.name = new RegExp(`${req.query.name}`, "i"); }
     
     // Determina il criterio di ordinamento
-    if (req.query.price_asc === "true")    { sort_criteria["min_price"] = 1; }
-    if (req.query.price_desc === "true")   { sort_criteria["min_price"] = -1; }
-    if (req.query.name_asc === "true")     { sort_criteria["name"] = 1; }
-    if (req.query.name_desc === "true")    { sort_criteria["name"] = -1; }
+    if (req.query.price_asc === "true")    { sort_criteria = { "min_price": 1, "relevance": -1, "_id": -1 }; }
+    if (req.query.price_desc === "true")   { sort_criteria = { "min_price": -1, "relevance": -1, "_id": -1 }; }
+    if (req.query.name_asc === "true")     { sort_criteria = { "name": 1, "relevance": -1, "_id": -1 }; }
+    if (req.query.name_desc === "true")    { sort_criteria = { "name": -1, "relevance": -1, "_id": -1 }; }
     
     try {
         items = await ItemModel.aggregate([
