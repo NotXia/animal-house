698ae75a7ee23c1b76b851455c672b68823c0631
Zaid1710 <zaid1234@hotmail.it>
2022-10-10 12:19:23 +0200

    Add get services

--- frontend/backoffice/manage/services/main.js
+++ frontend/backoffice/manage/services/main.js
@@ -1,13 +1,15 @@
 import { Navbar } from "/admin/import/Navbar.js";
 import { Loading } from "/admin/import/Loading.js";
 // import { Error } from "/admin/import/Error.js";
-// import { api_request } from "/js/auth.js";
+import { api_request } from "/js/auth.js";
 // import * as Form from "./form.js";
-// import * as Mode from "./mode.js";
-// import * as ServiceRow from "./view/serviceRow.js"
+import * as Mode from "./mode.js";
+import * as ServiceRow from "./view/serviceRow.js";
 
 let NavbarHandler;
 let LoadingHandler;
+
+let service_cache;
 
 $(async function () {
     // Caricamento delle componenti esterne
@@ -17,5 +19,44 @@ $(async function () {
 
     await LoadingHandler.wrap(async function () {
         await NavbarHandler.render();
+        
+        await showServices();
     })
 })
+
+/* Caricamento dei servizi */
+async function showServices() {
+    service_cache = await fetchServices();
+    displayServices(service_cache);
+}
+
+/* Estrae tutti i servizi */
+async function fetchServices() {
+    try {
+        let service = await api_request({
+            type: "GET", url: `/services/`
+        })
+
+        // Ordinamento alfabetico
+        service.sort((s1, s2) => s1.name.toLowerCase().localeCompare(s2.name.toLowerCase()));
+
+        return service;
+    } catch (err) {
+        Mode.error(err.responseJSON.message ? err.responseJSON.message : "Si è verificato un errore");
+    }
+}
+
+/**
+ * Mostra a schermo dei dati servizi
+ * @param serviceList   Servizi da visualizzare
+ */
+function displayServices(serviceList) {
+    $("#service-container").html("");
+    let index = 0;
+
+    for (const service of serviceList) {
+        $("#service-container").append(ServiceRow.render(service, index));
+
+        index++;
+    }
+}
---
+++ frontend/backoffice/manage/services/mode.js
@@ -0,0 +1,24 @@
+import * as Form from "./form.js";
+
+export const CREATE = 1,
+             MODIFY = 2,
+             ERROR = -1;
+
+export let current;
+
+// export function create() {
+//     current = CREATE;
+//     $("#modal-service-title").text("Crea servizio");
+//     Form.createMode();
+// }
+
+// export function modify() {
+//     current = MODIFY;
+//     $("#modal-service-title").text("Modifica servizio");
+//     Form.modifyMode();
+// }
+
+export function error(message) {
+    current = ERROR;
+    $("#global-feedback").html(message);
+}
