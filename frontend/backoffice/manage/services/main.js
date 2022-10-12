5b82561d158bbfccede666146b52c00def39b443
Zaid1710 <zaid1234@hotmail.it>
2022-10-12 13:12:03 +0200

    Add form insert service

--- frontend/backoffice/manage/services/main.js
+++ frontend/backoffice/manage/services/main.js
@@ -27,13 +27,14 @@ $(async function () {
 /* Caricamento dei servizi */
 async function showServices() {
     service_cache = await fetchServices();
+    displaySpecies();
     displayServices(service_cache);
 }
 
 /* Estrae tutti i servizi */
 async function fetchServices() {
     try {
-        let service = await api_request({
+        let service = await $.ajax({
             type: "GET", url: `/services/`
         })
 
@@ -58,5 +59,49 @@ function displayServices(serviceList) {
         $("#service-container").append(ServiceRow.render(service, index));
 
         index++;
+    }
+}
+
+/* Estrae le specie */
+async function fetchSpecies() {
+    try {
+        let species = await $.ajax({
+            type: "GET", url: `/animals/species/`
+        })
+
+        // Ordinamento alfabetico
+        species.sort((s1, s2) => s1.name.toLowerCase().localeCompare(s2.name.toLowerCase()));
+
+        return species;
+    } catch (err) {
+        Mode.error(err.responseJSON.message ? err.responseJSON.message : "Si è verificato un errore");
+    }
+}
+
+/* Mostra a schermo le specie */
+async function displaySpecies() {
+    try {
+        let speciesList = await fetchSpecies();
+        
+        for (const species of speciesList) {
+            let speciesLogo = "";
+            if (species.logo) {
+                speciesLogo = `<img src="data:image/*;base64,${species.logo}" alt="Icona per ${species.name}" class="species-icon" />`;
+            }
+
+            $("#data-target").append(`
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="data-${species.name}" name="target" aria-label="Specie ${species.name}">
+                <label for="data-${species.name}" class="fs-6 fw-semibold">
+                    ${speciesLogo}
+                    ${species.name}
+                </label>
+            </div>
+            `);
+        }
+        
+    } catch (err) {
+        console.log(err);
+        Mode.error(err.responseJSON.message ? err.responseJSON.message : "Si è verificato un errore");
     }
 }
