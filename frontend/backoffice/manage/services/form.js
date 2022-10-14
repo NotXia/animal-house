49c318bd5cc0a8c3aeddd6ef56ab920a5d0fdb84
Zaid1710 <zaid1234@hotmail.it>
2022-10-14 12:16:24 +0200

    Used priceToCents

--- frontend/backoffice/manage/services/form.js
+++ frontend/backoffice/manage/services/form.js
@@ -1,9 +1,11 @@
+import { priceToCents } from "/js/utilities.js";
+
 export function getServiceData() {
     return {
         name: $("#data-name").val(),
         description: $("#data-description").val(),
         duration: $("#data-duration").val(),
-        price: $("#data-price").val()*100,
+        price: priceToCents($("#data-price").val()),
         online: $("#data-online").is(":checked"),
         target: $.map($("input:checkbox[name=target]:checked"), (checked, _) => checked.value)
     }
