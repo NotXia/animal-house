d03fbd4d393f690218e0524bc7a65d15f15e41ba
NotXia <35894453+NotXia@users.noreply.github.com>
2022-11-01 13:50:21 +0100

    Add gestione paginazione

--- frontend/frontoffice/src/pages/Shop/Orders/index.jsx
+++ frontend/frontoffice/src/pages/Shop/Orders/index.jsx
@@ -10,29 +10,33 @@ import { isAuthenticated, getUsername, api_request } from "../../../import/auth.
 import OrderAPI from "../../../import/api/order.js"
 import OrderRow from "./components/OrderRow"
 
+const PAGE_SIZE = 5;
 
 class OrdersPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             orders: [],
+
+            curr_page: -1,
+            pagination_end: false,
+            fetching: false,
+
             error_message: ""
         };
+
+        this.fetch_request = null; // Richiesta di fetch degli ordini attuale
 
         isAuthenticated().then(is_auth => { if (!is_auth) { window.location = `${process.env.REACT_APP_BASE_PATH}/login?return=${window.location.href}` } } );
     }
 
     componentDidMount() {
-    (async () => {
-        try {
-            const orders = await OrderAPI.getMyOrders(20, 0);
-            this.setState({ orders: orders });
-        }
-        catch (err) {
-            console.log(err)
-            this.setState({ error_message: "Non è stato possibile caricare gli ordini" });
-        }
-    })()
+        (async () => {
+            this.setState({ fetching: true });
+            await this.fetchOrders();
+        })();
+
+        window.addEventListener("scroll", () => { this.scrollFetchOrders() });
     }
 
     render() {
@@ -48,6 +52,7 @@ class OrdersPage extends React.Component {
                     <Row>
                         <h1>Ordini effettuati</h1>
                     </Row>
+
                     {
                         this.state.orders.map((order) => (
                             <Row key={order.id} className="mb-5">
@@ -55,9 +60,52 @@ class OrdersPage extends React.Component {
                             </Row>
                         ))
                     }
+                    
+                    <Row className={this.state.fetching ? "d-block" : "d-none"}>
+                        <div className="d-flex justify-content-center mb-4">
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Caricamento degli ordini successivi</span>
+                            </div>
+                        </div>
+                    </Row>
                 </Container>
             </main>
         </>);
+    }
+
+    async fetchOrders() {
+        if (this.state.pagination_end) { return; } // Evita richieste vuote
+
+        try {
+            this.setState({ fetching: true });
+            
+            const orders = await OrderAPI.getMyOrders(PAGE_SIZE, this.state.curr_page+1);
+            this.setState({ 
+                orders: this.state.orders.concat(orders), 
+                curr_page: this.state.curr_page+1, 
+                pagination_end: (orders.length < PAGE_SIZE) 
+            });
+
+            this.setState({ fetching: false });
+        }
+        catch (err) {
+            this.setState({ error_message: "Non è stato possibile caricare gli ordini" });
+        }
+    }
+
+    async scrollFetchOrders() {
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scroll_percentage = winScroll / height;
+        
+        if (scroll_percentage > 0.7) {
+            if (!this.fetch_request) { // Per evitare richieste multiple
+                this.fetch_request = this.fetchOrders();
+            }
+
+            await this.fetch_request; // Attesa sulla richiesta corrente
+            this.fetch_request = null;
+        }
     }
 }
 
