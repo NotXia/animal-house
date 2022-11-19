96666377065b2aa4fddff799728e1738cf53ca92
NotXia <35894453+NotXia@users.noreply.github.com>
2022-11-19 17:16:32 +0100

    Add selettore slot orario

---
+++ frontend/frontoffice/src/pages/Booking/components/SlotSelector.jsx
@@ -0,0 +1,84 @@
+/**
+ * 
+ * Selettore della data
+ * 
+ * Attributi
+ * - hub            Codice dell'hub
+ * - service        Id del servizio
+ * 
+ * Callback
+ * - onSelected(slot)     Richiamato quando viene selezionata una data
+ * 
+ */
+
+import React from "react";
+import BookingAPI from "modules/api/booking";
+import moment from "moment";
+
+
+class SlotSelector extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            availabilities: [],
+            selected_date: "",
+
+            error_message: "",
+        };
+    }
+
+    render() {
+        return (
+            <div className="w-100">
+                <label className="w-100 text-center">
+                    Cerca disponibilità per il giorno
+                    <input type="date" className="w-100 text-center fs-5" min={new Date().toISOString().split("T")[0]} 
+                        onChange={(e) => this.fetchAvailabilities(e.target.value)} />
+                </label>
+
+                <div className="d-flex justify-content-center mt-2">
+                    <div>
+                        {
+                            this.getAvailableStartTimes().map((slot) => (
+                                <button key={slot} className="btn btn-outline-primary fs-5 p-2 px-4 m-2" onClick={() => this.handleSlotSelection(slot)} >
+                                    {moment(slot).format("HH:mm")}
+                                </button>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    async fetchAvailabilities(date) {
+        try {
+            date = moment(date, "YYYY-MM-DD").format()
+            const availabilities = await BookingAPI.getAvailabilitiesOfDate(date, this.props.hub, this.props.service);
+
+            this.setState({ availabilities: availabilities, selected_date: date });
+        }
+        catch (err) {
+            this.setState({ error_message: "Non è stato possibile trovare disponibilità" })
+        }
+    }
+
+    getAvailableStartTimes() {
+        let available_days = new Set();
+
+        this.state.availabilities.forEach((availability) => available_days.add(availability.time.start));
+
+        return Array.from(available_days);
+    }
+
+    handleSlotSelection(slot_start) {
+        const slots = this.state.availabilities.filter((slot) => slot.time.start === slot_start);
+        const selected_slot = slots[Math.floor(Math.random() * slots.length)]; // Se nello slot sono disponibili più operatori, ne viene selezionato uno casualmente
+
+        if (this.props.onSelected) {
+            this.props.onSelected(selected_slot);
+        }
+    }
+}
+
+export default SlotSelector;
---
+++ frontend/modules/api/booking.js
@@ -0,0 +1,20 @@
+import { api_request } from "../auth.js";
+import $ from "jquery";
+import { DOMAIN } from "../const";
+
+const BookingAPI = {
+    getAvailabilitiesOfDate: async function (date, hub_code, service_id) {
+        return await api_request({ 
+            method: "GET", 
+            url: `${DOMAIN}/appointments/availabilities/`,
+            data: {
+                start_date: date,
+                end_date: date,
+                hub_code: hub_code,
+                service_id: service_id
+            }
+        });
+    },
+}
+
+export default BookingAPI;
