05fc20a9555e2f9cae36ef254f93215990c06c80
NotXia <35894453+NotXia@users.noreply.github.com>
2022-11-19 17:41:14 +0100

    Remove log

--- frontend/frontoffice/src/pages/Booking/components/MapController.jsx
+++ frontend/frontoffice/src/pages/Booking/components/MapController.jsx
@@ -3,7 +3,6 @@ import { useMap } from "react-leaflet";
 export default function MapController(props) {
     const map = useMap();
 
-    console.log(props)
     if (props.setMap) {
         props.setMap(map);
     }
