5e289fe9b7eb6c2d360bf070503529ab25fd2d56
NotXia <35894453+NotXia@users.noreply.github.com>
2022-12-21 17:01:02 +0100

    Add visualizzazione appuntamenti ordinati

--- frontend/backoffice/manage/appointments/main.js
+++ frontend/backoffice/manage/appointments/main.js
@@ -18,10 +18,24 @@ $(async function () {
         await NavbarHandler.render();
 
         const appointments = await AppointmentsAPI.getAppointmentsByUsername(await getUsername());
-        console.log(appointments)
-        for (const appointment of appointments) {
-            $("#container-appointments").append(await renderAppointment(appointment));
+        appointments.sort( (a1, a2) => moment(a1.time_slot.start).diff(moment(a2.time_slot.start)) );
+        const appointment_today = appointments.filter((appointment) => moment(appointment.time_slot.start).isSame(moment(), "day"));
+        const appointment_tomorrow = appointments.filter((appointment) => moment(appointment.time_slot.start).isSame(moment().add(1, "day"), "day"));
+        const appointment_future = appointments.filter((appointment) => moment(appointment.time_slot.start).isSameOrAfter(moment().add(2, "day"), "day"));
+
+        if (appointment_today.length > 0) {
+            $("#container-appointments-today").append(`<h2 class="mt-3 mb-1" aria-label="Appuntamenti di oggi">Oggi</h2>`);
+            for (const appointment of appointment_today) { $("#container-appointments-today").append(await renderAppointment(appointment)); }
         }
 
-    })
+        if (appointment_tomorrow.length > 0) {
+            $("#container-appointments-tomorrow").append(`<h2 class="mt-3 mb-1" aria-label="Appuntamenti di domani">Domani</h2>`);
+            for (const appointment of appointment_tomorrow) { $("#container-appointments-tomorrow").append(await renderAppointment(appointment)); }
+        }
+
+        if (appointment_future.length > 0) {
+            $("#container-appointments-future").append(`<h2 class="mt-3 mb-1" aria-label="Appuntamenti futuri">Prossimamente</h2>`);
+            for (const appointment of appointment_future) { $("#container-appointments-future").append(await renderAppointment(appointment)); }
+        }
+    });
 })
