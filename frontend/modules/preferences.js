f85fcc2b0b31a3751536d146a35055bb704172b1
NotXia <35894453+NotXia@users.noreply.github.com>
2023-01-02 19:44:48 +0100

    Fix import mancante

--- frontend/modules/preferences.js
+++ frontend/modules/preferences.js
@@ -1,3 +1,4 @@
+import { getUsername } from "./auth";
 import AnimalAPI from "./api/animals";
 
 /**
@@ -22,6 +23,7 @@ export async function loadUserPreferences() {
         ));
     }
     catch (err) {
+        console.log(err)
     }
 }
 
