cdde47095cf8d02cc2942240090f42f6e8166f0f
NotXia <35894453+NotXia@users.noreply.github.com>
2023-01-04 15:45:26 +0100

    Fix accessibilità

--- frontend/frontoffice/src/pages/Vip/index.jsx
+++ frontend/frontoffice/src/pages/Vip/index.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Helmet } from 'react-helmet'
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "../../components/form/CheckoutForm";
 import { centToPrice } from "modules/currency";
 import moment from "moment";
 import Loading from "../../components/Loading";
@@ -13,21 +10,16 @@ import UserAPI from "modules/api/user";
 import { getUsername, isAuthenticated } from "modules/auth";
 
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
-
-
 class Homepage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            step: "", // [recap, overview, pay]
+            step: null, // [recap, overview]
             price: 0,
-            stripe_client_secret: null,
 
             error_message: ""
         };
 
-        this.payment = React.createRef();
         this.loading = React.createRef();
     }
 
@@ -58,7 +50,7 @@ class Homepage extends React.Component {
             
             <main className="mt-3">
                 {
-                    this.state.step !== "pay" &&
+                    this.state.step &&
                     <div className="container">
                         <div className="row text-center">
                             {
@@ -74,82 +66,47 @@ class Homepage extends React.Component {
                                 </>
                             }
                         </div>
+
                         <div className="row">
                             <div className="col-12 col-md-6 offset-md-3 border border-primary rounded py-2 text-center">
-                                <p className="fs-2 m-0 fw-semibold">VIP Animal House</p>
-                                <p className="fs-3 fw-semibold m-0">{centToPrice(this.state.price)}€</p>
-                                <p className="fs-5 muted">per un anno di abbonamento</p>
-                                
-                                <h2 className="fs-5 fw-semibold m-0 text-decoration-underline">Vantaggi</h2>
-                                <ul style={{ listStyleType: "none" }}>
-                                    <li className="fs-5 m-0">Sconti esclusivi per i tuoi acquisti nello shop</li>
-                                    <li className="fs-5 m-0">Riduzione del prezzo dei servizi che prenoti</li>
-                                    <li className="fs-5 m-0">Elaborazione prioritaria dei tuoi ordini</li>
-                                    <li className="fs-5 m-0">Un addetto dedicato a te quando vieni a trovarci</li>
-                                    <li className="fs-5 m-0">Un esclusivo badge per distinguerti nel forum</li>
-                                </ul>
+                                <section aria-label="Descrizione piano VIP">
+                                    <p className="fs-2 m-0 fw-semibold">VIP Animal House</p>
+                                    <p className="visually-hidden">{centToPrice(this.state.price)}€ per un anno di abbonamento</p>
+                                    <p className="fs-3 fw-semibold m-0" aria-hidden="true">{centToPrice(this.state.price)}€</p>
+                                    <p className="fs-5 muted" aria-hidden="true">per un anno di abbonamento</p>
+                                    
+                                    <h2 className="fs-5 fw-semibold m-0 text-decoration-underline">Vantaggi</h2>
+                                    <ul style={{ listStyleType: "none" }}>
+                                        <li className="fs-5 m-0">Sconti esclusivi per i tuoi acquisti nello shop</li>
+                                        <li className="fs-5 m-0">Riduzione del prezzo dei servizi che prenoti</li>
+                                        <li className="fs-5 m-0">Elaborazione prioritaria dei tuoi ordini</li>
+                                        <li className="fs-5 m-0">Un addetto dedicato a te quando vieni a trovarci</li>
+                                        <li className="fs-5 m-0">Un esclusivo badge per distinguerti nel forum</li>
+                                    </ul>
+                                </section>
 
-                                <div className="d-flex justify-content-center mb-2">
-                                    <button className="btn btn-primary" onClick={() => this.startPayment()}>
-                                    {
-                                        this.state.step === "overview" && "Ottieni VIP"
-                                    }
-                                    {
-                                        this.state.step === "recap" && "Rinnova VIP"
-                                    }
-                                    </button>
-                                </div>
+                                <section aria-label="Acquista il piano VIP">
+                                    <div className="d-flex justify-content-center mb-2">
+                                        <a className="btn btn-primary" href={`${process.env.REACT_APP_BASE_PATH}/vip/pay`}>
+                                        {
+                                            this.state.step === "overview" && "Ottieni VIP"
+                                        }
+                                        {
+                                            this.state.step === "recap" && "Rinnova VIP"
+                                        }
+                                        </a>
+                                    </div>
+                                </section>
                             </div>
                         </div>
                     </div>
                 }
 
-                {
-                    this.state.step === "pay" && this.state.stripe_client_secret && 
-                    <div className="container">
-                        <div className="row text-center">
-                            <h1 className="m-0">Pagamento</h1>
-                            <p className="fs-4 fw-semibold">{centToPrice(this.state.price)}€ per un anno di VIP</p>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-                                <Elements options={{ clientSecret: this.state.stripe_client_secret }} stripe={stripePromise}>
-                                    <CheckoutForm ref={this.payment} />
-                                </Elements>
-                            </div>
-                        </div>
-
-                        <div className="row mt-3">
-                            <div className="d-flex justify-content-center">
-                                <button className="btn btn-outline-success btn-lg" onClick={() => this.completePayment()}>Paga</button>
-                            </div>
-                        </div>
-                    </div>
-                }
             </main>
 
             <Footer />
             </>
         );
-    }
-
-    async startPayment() {
-        try {
-            const payment_data = await CustomerAPI.startVIPCheckout();
-
-            this.setState({ stripe_client_secret: payment_data.clientSecret, step: "pay" });
-        }
-        catch (err) {
-            this.setState({ error_message: "Non è stato possibile creare avviare il pagamento" });
-        }
-    }
-
-    async completePayment() {
-        this.loading.current.wrap(async () => {
-            await this.payment.current.handlePayment(`http://localhost:3000/fo/vip/success`);
-            // await this.payment.current.handlePayment(`${process.env.REACT_APP_DOMAIN}${process.env.REACT_APP_BASE_PATH}/vip/success`);
-        });
     }
 }
 
--- frontend/frontoffice/src/pages/Vip/routes.jsx
+++ frontend/frontoffice/src/pages/Vip/routes.jsx
@@ -1,10 +1,15 @@
 import VipPage from "./index";
+import PaymentPage from "./payment";
 import SuccessPage from "./success";
 
 let routes = [
     {
         path: "/vip",
         element: <VipPage />,
+    },
+    {
+        path: "/vip/pay",
+        element: <PaymentPage />,
     },
     {
         path: "/vip/success",
